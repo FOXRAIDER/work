d1f26b570e95e9e534887a42ed07f63975eaa480
FOXRAIDER <43128412+FOXRAIDER@users.noreply.github.com>
2019-08-05 12:59:04 +0300

    Update code

--- b_quest.rb
+++ b_quest.rb
@@ -1,4 +1,4 @@
 print "Какое ваше любимое число?\n"
 like_number = gets.chomp.to_i
 like_number+=1
-print "Может быть число ", like_number ," ваше любимое?" 
+print 'Может быть число ' + like_number.to_s + ' ваше любимое?' 
